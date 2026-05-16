c5e625c0dd5c6fb3e9eebfdef93704a309f1b196
Yang Huang <huangyangace@gmail.com>
2026-05-15 17:28:42 -0700

    Homework 1 submit

--- Day 1/secondcprogram.c
+++ Day 1/secondcprogram.c
@@ -1,9 +1,20 @@
+/* HW 1 - Task 5 */
+
+/* Standard Libraries */
 #include <stdio.h>
+
+/**** MAIN PROGRAM ****/
 int main() {
-    char s[100];
-    char t[100];
-    printf ("What is your name? ");
-    scanf("%s", s);
-    fgets(s,100,stdin);
-    printf("%s? That's a funny name!",t);
+    char s[100];        // s is used to "pause" the console so it doesn't exit too quickly
+    char t[100];        // t is used to store the user's name
+
+    printf ("What is your name?");     // prompt the user to enter the name
+    scanf("%s", t);         // read the user's name from the console ("stdin")
+    fgets(s,100,stdin);     // pause the console and still see it before it disappears
+
+    printf("%s? That's a funny name!",t);  // tell the user that their name is funny
+
+    fgets(s,100,stdin);     // pause the console and still see it before it disappears
+
+    return 0;   // return 0 to the operating system to indicate that the program finished successfully
 }
--- Day 1/thirdcprogram.c
+++ Day 1/thirdcprogram.c
@@ -1,33 +1,17 @@
-/* A First C Program - for Homework 1
- *
- * This code will start you off.  Modify it as instructed in Homework 1.
- * 
- * Put your name here: Yang Huang
- * CS-5008 Summer 2026
- */
-
+/* HW 1 - Task 5 */
 /* Standard Libraries */
 #include <stdio.h>
 
 /**** MAIN PROGRAM ****/
-
 int main() {
-
-    int x, y;                        // x and y will be the values entered by the user
+    int x, y, z;                        // x, y and z will be the values entered by the user
     char s[100];                    // s is used to "pause" the console so it doesn't exit too quickly
 
     /* Ask for a number */
     printf("Enter a number: ");     // prompt the user
     scanf("%d", &x);                // read a number from the console ("stdin")
     fgets(s,100,stdin);             // sadly, we have to remove the \n that scanf leaves in stdin
-    
-    /* Normally I would not put a comment on every line of code.
-       I just did it above because you are learning C for the first time.
-       I would only put a comment on a line if it was hard to understand or required that you
-       know something that is not entirely obvious by looking at the code.
-    */
 
-    /* Taks 3 */
     /* Ask for another number */
     printf("Enter another number: ");     // prompt the user
     scanf("%d", &y);                // read a number from the console ("stdin")
@@ -36,6 +20,17 @@ int main() {
     /* Print the sum */
     printf("The sum is %d\n", x + y);  // tell the user the sum of the two numbers
 
+    /*======================*/
+    /* Ask the third number */
+    printf("Enter the third number: ");     // prompt the user
+    scanf("%d", &z);                // read a number from the console ("stdin")
+    fgets(s,100,stdin);             // pause the console and still see it before it disappears
+
+    /* Print the sum */
+    printf("The sum is %d\n", (x + y) * z);  // tell the user the total of the three numbers
+
     /* Pause the console so we can look at it before it vanishes */
     fgets(s,100,stdin);
+    
+    return 0;   // return 0 to the operating system to indicate that the program finished successfully
 }

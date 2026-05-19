427b7b006890b665ba0ab96875e74ec0d7d75564
Angace <57075133+Angace@users.noreply.github.com>
2026-05-19 10:15:49 -0700

    add \n to change list

--- Day 1/secondcprogram.c
+++ Day 1/secondcprogram.c
@@ -12,7 +12,7 @@ int main() {
     scanf("%s", t);         // read the user's name from the console ("stdin")
     fgets(s,100,stdin);     // pause the console and still see it before it disappears
 
-    printf("%s? That's a funny name!",t);  // tell the user that their name is funny
+    printf("%s? That's a funny name!\n",t);  // tell the user that their name is funny
 
     fgets(s,100,stdin);     // pause the console and still see it before it disappears
 
@@ -24,5 +24,6 @@ int main() {
     2. Change scanf("%s", s) to scanf("%s", t)，s is to handle the console pause, so we want to read user input into t  
     3. Add one more fgets(s,100,stdin) after printf to pause the console, so the window stay on the screen until hit Enter to make it go away
     4. Add "return 0" at the end to indicate that the program finished successfully
+    5. Add "\n" at the end of the printf to make the output look nicer
     */
 }
